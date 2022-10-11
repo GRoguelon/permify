457c14b19082aa36254535ddd5fd9393b45ca21f
Geoffrey Roguelon <309354+GRoguelon@users.noreply.github.com>
2022-10-10 19:52:20 -0500

    Removed extra app

--- mix.exs
+++ mix.exs
@@ -23,9 +23,7 @@ defmodule Permify.MixProject do
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   def package do
